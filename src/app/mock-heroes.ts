d789f83204aa88e2fc7c93b2f9c4c61349397acf
shekhar-singh <sschoudhary60@gmail.com>
2019-08-08 12:20:07 +0530

    avengers update

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Iron Man' },
+  { id: 2, name: 'Hulk' },
+  { id: 3, name: 'Caption America' },
+  { id: 4, name: 'Spider Man' },
+  { id: 5, name: 'Ant man' },
+  { id: 6, name: 'Wasp' },
+  { id: 7, name: 'Natasa' },
+  { id: 8, name: 'Dr Strange' },
+  { id: 9, name: 'Black Panther' },
+  { id: 10, name: 'Hawk Eye' }
+];
